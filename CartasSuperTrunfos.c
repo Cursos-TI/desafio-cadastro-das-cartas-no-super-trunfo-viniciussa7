14131d135366b3784ba2f1a28e52c7d4093462cd
viniciussa7 <vinycius.j7@gmail.com>
2025-02-24 23:21:59 +0000

    Inicio do Super Trunfo.

---
+++ CartasSuperTrunfos.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Desafio SUPER TRUNFO
+// Cadsto de cartas
+// Codigo inicial para cadastramento de cartas de cidades
+
+int main (){
+    // simplificar:Defina as ordens das variaveis com espaço para umma melhor visualização.
+    // exemplo: inciar pelo nome do Estado em segida, codigo da carta,nome da cidade ,população, área, PIB, número de pontos turísticos
+
+    // Cadastro das Cartas:
+    // Solicite ao usuário que insira as informações que forem solicitadas dentro de cada variavel
+
+    // Exibição dos Dados das Cartas:
+    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    printf("SUPER TRUFO!\n");
+
+    char cidade[30]
+    char codigo da carta[20]
+    char nome da cidade [30]
+    int população
+    float área em km²
+    float PIB
+    int numero de pontos turisticos
+
+    
+
+
+
+
+
+
+
+
+
+
+}
